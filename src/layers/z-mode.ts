1f1b74df797238801cc8c6d81a102d79c2855c08
Evan Liu <evancoding@gmail.com>
2023-04-27 08:52:22 +1200

    🔧 Add more gitmoji to z-mode

--- src/layers/z-mode.ts
+++ src/layers/z-mode.ts
@@ -1,15 +1,18 @@
 import { layer, map } from 'karabiner.ts'
 
+// See https://gitmoji.dev/
 export const zMode = layer('z', 'z-mode').manipulators([
+  map('b').toPaste('👷'), // add or update ci Build system
+  map('c').toPaste('✅'), // _check
+  map('d').toPaste('🚀'), // Deploy stuff
+  map('f').toPaste('🐛'), // Fix a bug
+  map('h').toPaste('💯'), // _hundred
+  map('j').toPaste('😂'), // _joy
   map('m').toPaste('🔀'), // Merge branches
-  map('n').toPaste('✨'), // Introduce New features
-  map('v').toPaste('🔖'), // Release / Version tags
-
-  map('j').toPaste('😂'), // (j)oy
-  map('i').toPaste('😅'), // sweat_sm(i)le
-  map('u').toPaste('👍'), // thumbs(u)p
-
-  map('c').toPaste('✅'), // Check
-  map('e').toPaste('❗️'), // Exclamation
-  map('k').toPaste('❓'), // (k)question
+  map('n').toPaste('✨'), // introduce New features
+  map('p').toPaste('👍'), // _plus_one +1
+  map('s').toPaste('😅'), // _sweat_smile
+  map('t').toPaste('🧪'), // _test
+  map('u').toPaste('⬆️'), // Upgrade dependencies
+  map('v').toPaste('🔖'), // release / Version tags
 ])
