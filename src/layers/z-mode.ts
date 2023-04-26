555f901cf79b1cdb88f13b81eea3d37af5def054
Evan Liu <evancoding@gmail.com>
2023-04-26 21:45:38 +1200

    🔧 Map v to 🔖 in z-mode

--- src/layers/z-mode.ts
+++ src/layers/z-mode.ts
@@ -3,6 +3,7 @@ import { layer, map } from 'karabiner.ts'
 export const zMode = layer('z', 'z-mode').manipulators([
   map('m').toPaste('🔀'), // Merge branches
   map('n').toPaste('✨'), // Introduce New features
+  map('v').toPaste('🔖'), // Release / Version tags
 
   map('j').toPaste('😂'), // (j)oy
   map('i').toPaste('😅'), // sweat_sm(i)le
