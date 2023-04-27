e64726e0983a5662d29e23f6ab83ca14cb07c1d9
Evan Liu <evancoding@gmail.com>
2023-04-27 20:12:54 +1200

    🔧 Update d and r on z-mode

--- src/layers/z-mode.ts
+++ src/layers/z-mode.ts
@@ -4,13 +4,14 @@ import { layer, map } from 'karabiner.ts'
 export const zMode = layer('z', 'z-mode').manipulators([
   map('b').toPaste('👷'), // add or update ci Build system
   map('c').toPaste('✅'), // _check
-  map('d').toPaste('🚀'), // Deploy stuff
+  map('d').toPaste('📝'), // add or update Documentation
   map('f').toPaste('🐛'), // Fix a bug
   map('h').toPaste('💯'), // _hundred
   map('j').toPaste('😂'), // _joy
   map('m').toPaste('🔀'), // Merge branches
   map('n').toPaste('✨'), // introduce New features
   map('p').toPaste('👍'), // _plus_one +1
+  map('r').toPaste('♻️'), // Refactor code
   map('s').toPaste('😅'), // _sweat_smile
   map('t').toPaste('🧪'), // _test
   map('u').toPaste('⬆️'), // Upgrade dependencies
