b33dc343ab6479c1663fad8b8f33d8d72d96d057
Evan Liu <evancoding@gmail.com>
2023-08-07 22:39:01 +1200

    🔧 Change Hyper/Meh keys to right ⌘⌥ on Apple keyboard

--- src/devices/apple-keyboard.ts
+++ src/devices/apple-keyboard.ts
@@ -5,7 +5,6 @@ const ifAppleKeyboard = ifDevice({ vendor_id: 1452, product_id: 835 })
 export const appleKeyboard = rule('Apple', ifAppleKeyboard).manipulators([
   map('⇪').to('⎋'),
 
-  // The esc on the keyboard, not the ⇪ mapping above
-  map('⎋', 'fn').toHyper(),
-  map('⎋', '⌃').toMeh(),
+  map('›⌘').toHyper(),
+  map('›⌥').toMeh(),
 ])
