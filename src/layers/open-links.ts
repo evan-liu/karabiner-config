04d9567f62dac992e6b659e057d9071c48035391
Evan Liu <evancoding@gmail.com>
2023-09-22 11:32:09 +1200

    🔧 Add links

--- src/layers/open-links.ts
+++ src/layers/open-links.ts
@@ -8,6 +8,7 @@ export const openLinks = {
   w: toArc('https://www.metservice.com/towns-cities/locations/auckland'), // Weather
   g: toArc('https://github.com/evan-liu/karabiner.ts'),
   j: toArc(`https://${workJiraOrg}.atlassian.net/browse/${workJiraProj}`),
+  c: toArc('https://chat.openai.com/'),
   m: toArc('https://mail.google.com'),
 }
 
