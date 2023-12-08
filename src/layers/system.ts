40c3b14537c652f037a39c8ba1d49740312c52e4
Evan Liu <evancoding@gmail.com>
2023-12-08 16:28:24 +1300

    🔧 Update mouse cursor mappings

--- src/layers/system.ts
+++ src/layers/system.ts
@@ -5,7 +5,7 @@ export const system = {
   1: toMouseCursorPosition({ x: '25%', y: '50%', screen: 0 }),
   2: toMouseCursorPosition({ x: '50%', y: '50%', screen: 0 }),
   3: toMouseCursorPosition({ x: '75%', y: '50%', screen: 0 }),
-  0: toMouseCursorPosition({ x: '50%', y: '50%', screen: 1 }),
+  4: toMouseCursorPosition({ x: '50%', y: '50%', screen: 1 }),
 
   '⏎': toPointingButton('button1'),
 
