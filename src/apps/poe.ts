dad798714bf6f845aecbb36e71f3e56622380f03
Evan Liu <evancoding@gmail.com>
2024-01-17 09:55:22 +1300

    🔧 Add ',' to open System Settings on system layer

---
+++ src/apps/poe.ts
@@ -0,0 +1,5 @@
+import { ifApp } from 'karabiner.ts'
+
+export const isPoe = ifApp(`^com.quora.poe.electron$`)
+
+export const poe = {}
--- src/layers/system.ts
+++ src/layers/system.ts
@@ -1,13 +1,17 @@
-import { toMouseCursorPosition, toPointingButton } from 'karabiner.ts'
+import { toApp, toMouseCursorPosition, toPointingButton } from 'karabiner.ts'
 import { toClearNotifications } from '../utils/to-clear-notifications'
 
 export const system = {
   1: toMouseCursorPosition({ x: '25%', y: '50%', screen: 0 }),
   2: toMouseCursorPosition({ x: '50%', y: '50%', screen: 0 }),
   3: toMouseCursorPosition({ x: '75%', y: '50%', screen: 0 }),
-  4: toMouseCursorPosition({ x: '50%', y: '50%', screen: 1 }),
+  4: toMouseCursorPosition({ x: '99%', y: 20, screen: 0 }),
+
+  5: toMouseCursorPosition({ x: '50%', y: '50%', screen: 1 }),
 
   '⏎': toPointingButton('button1'),
 
   n: toClearNotifications(),
+
+  ',': toApp('System Settings'),
 }
