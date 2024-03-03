b8b717538f781d1598dfc7da54fb39189d4115dd
Evan Liu <evancoding@gmail.com>
2024-03-04 10:26:07 +1300

    ♻️ Refactor duoModifier()

--- src/utils/duo-modifier.ts
+++ src/utils/duo-modifier.ts
@@ -18,12 +18,12 @@ export function duoModifier(
 ) {
   const id = keys + modifier
 
-  const parsedModifiers =
+  const [firstMod, ...restMods] =
     modifier in modifierKeyAliases
       ? ([modifierKeyAliases[modifier as ModifierKeyAlias]] as Mod[])
       : (multiModifierAliases[modifier as MultiModifierAlias] as Mod[])
   const to_after_key_up = [toRemoveNotificationMessage(id)]
   return mapSimultaneous(keys.split('') as Key[], { to_after_key_up })
     .toNotificationMessage(id, modifier) // Must go first or to() doesn't work
-    .to(`left_${parsedModifiers[0]}`, parsedModifiers.slice(1))
+    .to(`left_${firstMod}`, restMods)
 }
