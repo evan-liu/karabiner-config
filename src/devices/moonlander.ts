161a3673e8e9ceedbe8c628d547e5bd21bed8661
Evan Liu <evancoding@gmail.com>
2024-06-05 15:48:34 +1200

    Sync dotfiles

--- src/devices/moonlander.ts
+++ src/devices/moonlander.ts
@@ -4,4 +4,5 @@ export const ifMoonlander = ifDevice({ vendor_id: 12951, product_id: 6505 })
 
 export const moonlander = rule('Moonlander', ifMoonlander).manipulators([
   map('⎋', '⇧').to('⇪'),
+  map('⎋', '⇪').to('⇪'),
 ])
